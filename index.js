427dbe8b9ddae7897f092aa8f781eb3c62b9723c
Erika Kavuto <erika.gwiyo@student.moringaschool.com>
2026-04-16 13:58:37 +0300

    Asyn/await lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+function fetchAndRenderPosts() {
+    // 👇 Mocked data (same as API response)
+    const post = {
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum"
+    };
+  
+    const ul = document.getElementById('post-list');
+  
+    const li = document.createElement('li');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+  
+    h1.textContent = post.title;
+    p.textContent = post.body;
+  
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  }
+  
+  fetchAndRenderPosts();
